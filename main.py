4b50a0143131ddb6afd1b556b709edcb718801f0
talm <talm@wustl.edu>
2019-06-09 16:30:01 +0300

    removed comments

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from create_config import get_config, set_config
 from experiment import Experiment
 
 
-def main():
-    config_path = set_config(config_name='BUD')
-    # config_path = r'C:\Users\User\PycharmProjects\GiMiCHI\configs'
-    # config_path = r'/Users/montequie/Dropbox/IDC - CS/miLAB/BUD_BUTTER/configs'
-    # config = get_config(os.path.join(config_path, 'config.json'))
-    # config = get_config(os.path.join(config_path, 'config_BUD.json'))
-
+def main(config_path=None):
+    if not config_path:
+        config_path = set_config(config_name='BUD')
+    # TODO: ask Dina for stages
     stages = []
     experiment = Experiment(name='BUD', stages=stages)
     experiment.run(get_config(config_path))
@@ -16,3 +13,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # main(config_path=r'C:\Users\User\PycharmProjects\GiMiCHI\configs')
