4ef63c91b98d4af954371544ca37921e78a3297a
montequie <tal.moran@milab.idc.ac.il>
2019-06-10 15:12:14 +0300

    some changes

--- create_config.py
+++ create_config.py
@@ -1,35 +1,48 @@
 import os
 import json
-
+3
 GOAL_POSITION_REGISTER = 'goal_position'
-# TODO: test for correct name
 MOVING_SPEED_REGISTER = 'moving_speed'
-# TODO: test for correct name
 GOAL_ACCELERATION_REGISTER = 'goal_acceleration'
 
-config_path = r'C:\Users\User\PycharmProjects\GiMiCHI\configs'
 config_path = r'/Users/montequie/Dropbox/IDC - CS/miLAB/BUD/configs'
+config_path = r'C:\Users\User\PycharmProjects\BUD\configs'
 
 motor_turn_names = ["turnone", "turntwo", "turnthree", "turnfour"]
 motor_lean_names = ["leanone", "leantwo", "leanthree", "leanfour"]
 
 fixed_position_dict = {}
 for motor_turn_name in motor_turn_names:
-    fixed_position_dict[motor_turn_name] = {GOAL_POSITION_REGISTER: 0,
-                                            MOVING_SPEED_REGISTER: 1,
-                                            GOAL_ACCELERATION_REGISTER: 1}
+    fixed_position_dict[motor_turn_name] = {GOAL_ACCELERATION_REGISTER: 10,
+                                            MOVING_SPEED_REGISTER: 300,
+                                            GOAL_POSITION_REGISTER: 0,
+                                            }
+# for motor_turn_name in motor_lean_names:
+#     fixed_position_dict[motor_turn_name] = {GOAL_ACCELERATION_REGISTER: 10,
+#                                             MOVING_SPEED_REGISTER: 300,
+#                                             GOAL_POSITION_REGISTER: 2000,
+#                                             }
 
 
 def set_config(config_name='config'):
     config = {}
-    config['IP'] = '192.168.0.106'
+    config['IP'] = '192.168.0.107'
     config['MOTOR_NAMES'] = motor_turn_names + motor_lean_names
-    # TODO: add real animations
-    play_animation_key = {'key': 'a',
+    play_Attentive_44_key = {'key': '1',
+                             'module': 'experiment',
+                             'function': '_play_animation',
+                             'help': 'play animation - Attentive_44',
+                             'args': 'Attentive_44'}
+    play_Follow_44_key = {'key': '2',
                           'module': 'experiment',
                           'function': '_play_animation',
-                          'help': 'play animation - test',
-                          'args': 'test'}
+                          'help': 'play animation - Follow_44',
+                          'args': 'Follow_44'}
+    play_Welcome_New_key = {'key': '3',
+                            'module': 'experiment',
+                            'function': '_play_animation',
+                            'help': 'play animation - Welcome_New',
+                            'args': 'Welcome_New'}
     torque_key = {'key': 't',
                   'module': 'experiment',
                   'function': '_disable_torque',
@@ -41,7 +54,8 @@ def set_config(config_name='config'):
                          'help': 'fix positions for all turn motors to 0 with minimum speed and acceleration',
                          # TODO: fix also the lean ones?
                          'args': fixed_position_dict}
-    config['SHORTCUTS'] = [play_animation_key, torque_key, fix_positions_key]
+    config['SHORTCUTS'] = [play_Attentive_44_key, play_Follow_44_key, play_Welcome_New_key, torque_key,
+                           fix_positions_key]
     with open(os.path.join(config_path, F'{config_name}.json'), 'w') as f:
         json.dump(config, f)
     return os.path.join(config_path, F'{config_name}.json')
--- experiment.py
+++ experiment.py
@@ -1,16 +1,11 @@
 import time
+
 import keyboard
 from butter.mas.api import HttpClient
 
 TORQUE_REGISTER = 'torque_enable'
 TORQUE_OFF = '0'
 TORQUE_ON = '1'
-
-GOAL_POSITION_REGISTER = 'goal_position'
-# TODO: test for correct name
-MOVING_SPEED_REGISTER = 'moving_speed'
-# TODO: test for correct name
-GOAL_ACCELERATION_REGISTER = 'goal_acceleration'
 
 CW_ANGLE_LIMIT_REGISTER = 'cw_angle_limit'
 CCW_ANGLE_LIMIT_REGISTER = 'ccw_angle_limit'
@@ -42,14 +37,15 @@ def _fix_goal_position(fixed_positions):
     2. moving to goal fixed positions minimum speed
     :param fixed_positions: dict, keys are motor names, values are dicts of registers and value
     '''
-    # TODO: make sure the order of toggle modes are correct
-    _set_joint_mode(list(fixed_positions.keys()))
-    _set_multi_turn_mode(list(fixed_positions.keys()))
-    for motor_name in fixed_positions.keys():
-        for register_name in fixed_positions[motor_name].keys():
-            # TODO: make sure the acceleration is needed
-            butterHttpClient.setMotorRegister(motor_name, register_name,
-                                              str(fixed_positions[motor_name][register_name]))
+    if _are_you_sure('fix goal position'):
+        _set_joint_mode(list(fixed_positions.keys()))
+        _set_multi_turn_mode(list(fixed_positions.keys()))
+        for motor_name in fixed_positions.keys():
+            for register_name in fixed_positions[motor_name].keys():
+                # TODO: make sure the acceleration is needed
+                butterHttpClient.setMotorRegister(motor_name, register_name,
+                                                  str(fixed_positions[motor_name][register_name]))
+        print('finished fixing')
 
 
 def _disable_torque(motor_names):
@@ -73,11 +69,19 @@ def _are_you_sure(action_name):
     time.sleep(0.01)
     print(F'{action_name}\n')
     return True
+
     # run_animation = input(F'are you sure you want to {action_name}? y/n\n')
     # if run_animation.lower() == 'y':
     #     return True
     # else:
     #     return False
+
+def _help(config):
+    print('Shortcut keys available:')
+    for shortcut in config['SHORTCUTS']:
+        key = shortcut['key']
+        help = shortcut['help']
+        print(f'\r{key} -> {help}')
 
 
 class Experiment:
@@ -102,17 +106,12 @@ class Experiment:
     def _quit_experiment(self):
         self.state = False
 
-    def _help(self):
-        print('Shortcut keys available:')
-        for shortcut in self.config['SHORTCUTS']:
-            print('\r{shortcut[key]} -> {shortcut[help]}')
-
     def init_keyboard_shortcuts(self, config=None):
         for shortcut in config['SHORTCUTS']:
             import_module = __import__(shortcut['module'])
             key = shortcut['key']
             function_name = shortcut['function']
             args = shortcut['args']
-            keyboard.add_hotkey(key, getattr(import_module, function_name), args=args)
-        keyboard.add_hotkey('q', self._quit_experiment())
-        keyboard.add_hotkey('h', self._help())
+            keyboard.add_hotkey(key, getattr(import_module, function_name), args=(args,))
+        # keyboard.add_hotkey('q', self._quit_experiment())
+        keyboard.add_hotkey('h', getattr(import_module, '_help'), args=(config, ))
