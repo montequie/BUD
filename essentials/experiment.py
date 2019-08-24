90cddf2855e78c5105c4e62520268dd0c7c62039
talm <talmontequie@gmail.com>
2019-08-24 15:57:46 +0300

    added name to Experiment for generic experiments

--- essentials/experiment.py
+++ essentials/experiment.py
@@ -7,9 +7,10 @@ from essentials.dynamixel_basics import Dynamixel
 
 
 class Experiment(Dynamixel):
-    def __init__(self, config):
+    def __init__(self, config, name=None):
         self.state = True
         self.config = config
+        self.name = name
         super(Experiment, self).__init__(ip=config['IP'], motor_names=config['MOTOR_NAMES'])
 
     def run(self):
